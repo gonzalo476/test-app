a7668fdf0caa1df0ee337079b6ca9614bfcdcc19
GONZALO ROJAS <rojasg476@gmail.com>
2020-12-15 18:55:44 -0600

    xd
    xd

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{ useState, useRef } from 'react';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+
+import ButtonStack from "./HomeStack/ButtonStack"
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ButtonStack/>
     </View>
   );
 }
@@ -15,7 +17,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center", 
   },
 });
